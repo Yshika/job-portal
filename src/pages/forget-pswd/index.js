facfa6a38f00b8e168da526e64b5f860d5b29f2a
Yshika Agarwal <yshika0581@gmail.com>
2022-01-08 22:51:02 +0530

    forget password page

--- src/pages/forget-pswd/index.js
+++ src/pages/forget-pswd/index.js
@@ -2,7 +2,30 @@ import React from "react";
 import "./style.scss";
 
 const ForgetPswd = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="dark-bg"></div>
+      <div className="frgt-pswd-main">
+        <div className="frgt-pswd-box">
+          <div className="text title color-dark">Forgot your password?</div>
+          <div className="label">
+            Enter the email associated with your account and we’ll send you
+            instructions to reset your password.
+          </div>
+          <div className="label color-dark">Email Address</div>
+          <input
+            placeholder="Enter your email"
+            name="email"
+            type="email"
+            className="login-input"
+          ></input>
+          <div className="btn">
+            <button className="title btn-login">Submit</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ForgetPswd;
